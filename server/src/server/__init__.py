a2ea88dc039b9e03be09e081a68f0ef70d70b6df
ODDOZ <antoine.oddoz@proton.me>
2026-09-14 10:56:28 +0200

    setup api routes

--- server/src/server/__init__.py
+++ server/src/server/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -13,6 +13,49 @@ def hello() -> tuple[object, int]:
     """Temporary route: replace or keep it while building the API."""
     return jsonify({"message": "Hello, world!"}), 200
 
+
+@app.get("/notes")
+def get_notes():
+    return jsonify([
+                   {"id": 1, "title": "Liste de courses", "content": "Lait, riz et pâtes"},
+                   {"id": 2, "title": "Flask", "content": "Apprendre les routes et le JSON."}
+               ]), 200
+
+
+@app.get("/notes/<int:id>")
+def get_note(id):
+    print(id)
+    return jsonify({"id": 1, "title": "Liste de courses", "content": "Lait, riz et pâtes"})
+
+@app.post("/notes")
+def create_note():
+    data = request.get_json(silent=True)
+
+    if not data:
+       return {"error": "JSON invalide ou manquant"}, 400
+
+    print(data.get('title'))
+    print(data.get('content'))
+       
+    return data, 201
+
+@app.put("/notes/<int:id>")
+def modify_note(id):
+    data = request.get_json(silent=True)
+    if not data:
+       return {"error": "JSON invalide ou manquant"}, 400
+
+    print(id)
+    print(data.get("title"))
+    print(data.get("content"))
+
+    return data, 200
+
+@app.delete("/notes/<int:id>")
+def delete_note(id):
+    print(id)
+
+    return '', 204
 
 def main() -> None:
     app.run(debug=True)
